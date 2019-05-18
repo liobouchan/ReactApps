3dba108dab2087581eaa379f45bd558e5fffd382
Leonardo <liogothicvampire@gmail.com>
2019-05-18 18:44:24 -0500

    Changes on views components

--- coreUIReact/src/views/AllGood/GraphViewer/GraphViewer.js
+++ coreUIReact/src/views/AllGood/GraphViewer/GraphViewer.js
@@ -40,9 +40,20 @@ class GraphViewer extends Component {
 
     this.toggle = this.toggle.bind(this);
     this.toggleFade = this.toggleFade.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.state = {
       collapse: true,
-      regulation : "Regulación Bancaria"
+      regulation : "Regulación Bancaria",
+      articleValue : "",
+      articleName : "",
+      articleDescription : "",
+      articleContent: "",
+      title: "",
+      chapter: "",
+      section: "",
+      apart: "",
+      subApart: ""
     }
   }
 
@@ -52,6 +63,43 @@ class GraphViewer extends Component {
 
   toggleFade() {
     this.setState((prevState) => { return { fadeIn: !prevState }});
+  }
+
+  handleInputChange(editEvent){
+    this.setState({
+      articleValue: editEvent.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    console.log('Click en Buscar');
+    event.preventDefault(); 
+    console.log(this.state.articleValue);
+    let articleToSearch = this.state.articleValue
+    fetch("http://localhost:8080/articles/"+articleToSearch)
+    .then(response => response.json())
+    .then(data => {
+      console.log("Dato del articulo: " , data.article);
+      if(data.article.nombre == "null"){data.article.nombre = ""}
+      if(data.article.descripcion == "null"){data.article.descripcion = ""}
+      if(data.article.contenido == "null"){data.article.contenido = ""}
+      if(data.article.titulo == "null"){data.article.titulo = ""}
+      if(data.article.capitulo == "null"){data.article.capitulo = ""}
+      if(data.article.seccion == "null"){data.article.seccion = ""}
+      if(data.article.apartado == "null"){data.article.apartado = ""}
+      if(data.article.subApartado == "null"){data.article.subApartado = ""}
+      this.setState({
+        articleName: data.article.nombre,
+        articleDescription : data.article.descripcion,
+        articleContent: data.article.contenido,
+        title: data.article.titulo,
+        chapter: data.article.capitulo,
+        section: data.article.seccion,
+        apart: data.article.apartado,
+        subApart: data.article.subApartado
+      })
+    })
+
   }
   
   loading = () => <div className = "animated fadeIn pt-1 text-center"> Loading... </div>
@@ -71,13 +119,13 @@ class GraphViewer extends Component {
                 Búsqueda de Información
               </CardHeader>
               <CardBody>
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                   <FormGroup row>
                     <Col md={12} xl={12} lg={12}>
                       <InputGroup>
-                        <Input type="text" id="articulo" name="input-obligacion" placeholder="Artículo"/>
+                        <Input type="text" id="articulo" name="input-articulo" placeholder="Artículo" value={this.state.articleValue} onChange={this.handleInputChange}/>
                           <InputGroupAddon addonType="append">
-                            <Button type="button" color="primary"><i className="fa fa-search"></i> Buscar</Button>
+                            <Button type="submit" color="primary"><i className="fa fa-search"></i> Buscar</Button>
                           </InputGroupAddon>
                       </InputGroup>
                     </Col>
@@ -89,7 +137,7 @@ class GraphViewer extends Component {
                     <Label htmlFor="textarea-input">Nombre :</Label>
                   </Col>
                   <Col xs="12" md="9">
-                    <Input type="textarea" name="textarea-input" id="textoNombre" rows="1" placeholder="Nombre del Artículo"/>
+                    <Input type="textarea" name="textarea-input" id="textoNombre" rows="1" placeholder="Nombre del Artículo" value={this.state.articleName}/>
                   </Col>
                 </FormGroup>
                 {/* Descripcion */}
@@ -98,7 +146,7 @@ class GraphViewer extends Component {
                     <Label htmlFor="textarea-input">Descripción :</Label>
                   </Col>
                   <Col xs="12" md="9">
-                    <Input type="textarea" name="textarea-input" id="textoDescripcion" rows="1" placeholder="Descripción del Artículo"/>
+                    <Input type="textarea" name="textarea-input" id="textoDescripcion" rows="1" placeholder="Descripción del Artículo" value={this.state.articleDescription}/>
                   </Col>
                 </FormGroup>
                 {/* Contenido */}
@@ -107,7 +155,7 @@ class GraphViewer extends Component {
                     <Label htmlFor="textarea-input">Contenido :</Label>
                   </Col>
                   <Col xs="12" md="9">
-                    <Input type="textarea" name="textarea-input" id="textoContenido" rows="9" placeholder="Contenido del Artículo"/>
+                    <Input type="textarea" name="textarea-input" id="textoContenido" rows="9" placeholder="Contenido del Artículo" value={this.state.articleContent}/>
                   </Col>
                 </FormGroup>
                 {/* Card De Pertenencia */}
@@ -125,7 +173,7 @@ class GraphViewer extends Component {
                           <Label htmlFor="text-input">Título :</Label>
                         </Col>
                         <Col xs="12" md="9">
-                          <Input type="text" name="text-input" id="textoTitulo" placeholder="Título de pertenencia"/>
+                          <Input type="text" name="text-input" id="textoTitulo" placeholder="Título de pertenencia" value={this.state.title}/>
                         </Col>
                       </FormGroup>
                       {/* Capítulo */}
@@ -134,7 +182,7 @@ class GraphViewer extends Component {
                           <Label htmlFor="text-input">Capítulo :</Label>
                         </Col>
                         <Col xs="12" md="9">
-                          <Input type="text" name="text-input" id="textoCapitulo" placeholder="Capítulo de pertenencia"/>
+                          <Input type="text" name="text-input" id="textoCapitulo" placeholder="Capítulo de pertenencia" value={this.state.chapter}/>
                         </Col>
                       </FormGroup>
                       {/* Sección */}
@@ -143,7 +191,7 @@ class GraphViewer extends Component {
                           <Label htmlFor="text-input">Sección :</Label>
                         </Col>
                         <Col xs="12" md="9">
-                          <Input type="text" name="text-input" id="textoSeccion" placeholder="Sección de pertenencia"/>
+                          <Input type="text" name="text-input" id="textoSeccion" placeholder="Sección de pertenencia" value={this.state.section}/>
                         </Col>
                       </FormGroup>
                       {/* Apartado */}
@@ -152,7 +200,7 @@ class GraphViewer extends Component {
                           <Label htmlFor="text-input">Apartado :</Label>
                         </Col>
                         <Col xs="12" md="9">
-                          <Input type="text" name="text-input" id="textoApartado" placeholder="Apartado de pertenencia"/>
+                          <Input type="text" name="text-input" id="textoApartado" placeholder="Apartado de pertenencia" value={this.state.apart}/>
                         </Col>
                       </FormGroup>
                       {/* subApartado */}
@@ -161,7 +209,7 @@ class GraphViewer extends Component {
                           <Label htmlFor="text-input">Sub Apartado :</Label>
                         </Col>
                         <Col xs="12" md="9">
-                          <Input type="text" name="text-input" id="textoSubApartado" placeholder="Sub Apartado de pertenencia"/>
+                          <Input type="text" name="text-input" id="textoSubApartado" placeholder="Sub Apartado de pertenencia" value={this.state.subApart}/>
                         </Col>
                       </FormGroup>
                     </CardBody>
--- coreUIReact/src/views/AllGood/GraphViewer/Grapho.js
+++ coreUIReact/src/views/AllGood/GraphViewer/Grapho.js
@@ -6,138 +6,138 @@ var neo4j = require('neo4j-driver').v1;
 var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "123"));
 
 // graph payload (with minimalist structure)
-const data = {
-  "nodes": [
-    {
-      "id": 2710,
-      "nombre": "Artículo 225"
-    },
-    {
-      "id": 1211,
-      "nombre": "Artículo 2 Bis 117 n"
-    },
-    {
-      "id": 0,
-      "nombre": "Anexo 1"
-    },
-    {
-      "id": 1202,
-      "nombre": "Artículo 2 Bis 117 j"
-    },
-    {
-      "id": 409,
-      "nombre": "Artículo 2 Bis 5"
-    },
-    {
-      "id": 1210,
-      "nombre": "De la constitución del porcentaje adicional del Suplemento de Conservación de"
-    },
-    {
-      "id": 579,
-      "nombre": "Artículo 2 Bis 36"
-    },
-    {
-      "id": 640,
-      "nombre": "Artículo 2 Bis 37"
-    },
-    {
-      "id": 562,
-      "nombre": "Artículo 2 Bis 22"
-    },
-    {
-      "id": 599,
-      "nombre": "Cobertura de riesgo de crédito"
-    },
-    {
-      "id": 928,
-      "nombre": "Artículo 2 Bis 90"
-    },
-    {
-      "id": 496,
-      "nombre": "Artículo 2 Bis 17"
-    }
-  ],
-  "links": [
-    {
-      "id": 4800,
-      "source": 2710,
-      "type": "REFIERE",
-      "target": 1211
-    },
-    {
-      "id": 4398,
-      "source": 1211,
-      "type": "REFIERE",
-      "target": 0
-    },
-    {
-      "id": 4390,
-      "source": 1202,
-      "type": "REFIERE",
-      "target": 1211
-    },
-    {
-      "id": 3988,
-      "source": 409,
-      "type": "REFIERE",
-      "target": 1211
-    },
-    {
-      "id": 1330,
-      "source": 1210,
-      "type": "TIENE",
-      "target": 1211
-    },
-    {
-      "id": 4085,
-      "source": 579,
-      "type": "REFIERE",
-      "target": 640
-    },
-    {
-      "id": 4088,
-      "source": 640,
-      "type": "REFIERE",
-      "target": 0
-    },
-    {
-      "id": 4084,
-      "source": 579,
-      "type": "REFIERE",
-      "target": 640
-    },
-    {
-      "id": 4053,
-      "source": 562,
-      "type": "REFIERE",
-      "target": 640
-    },
-    {
-      "id": 4052,
-      "source": 562,
-      "type": "REFIERE",
-      "target": 640
-    },
-    {
-      "id": 633,
-      "source": 599,
-      "type": "TIENE",
-      "target": 640
-    },
-    {
-      "id": 4227,
-      "source": 928,
-      "type": "REFIERE",
-      "target": 496
-    },
-    {
-      "id": 4046,
-      "source": 496,
-      "type": "REFIERE",
-      "target": 0
-    }
-  ]
-}
+// const data = {
+//   "nodes": [
+//     {
+//       "id": 2710,
+//       "nombre": "Artículo 225"
+//     },
+//     {
+//       "id": 1211,
+//       "nombre": "Artículo 2 Bis 117 n"
+//     },
+//     {
+//       "id": 0,
+//       "nombre": "Anexo 1"
+//     },
+//     {
+//       "id": 1202,
+//       "nombre": "Artículo 2 Bis 117 j"
+//     },
+//     {
+//       "id": 409,
+//       "nombre": "Artículo 2 Bis 5"
+//     },
+//     {
+//       "id": 1210,
+//       "nombre": "De la constitución del porcentaje adicional del Suplemento de Conservación de"
+//     },
+//     {
+//       "id": 579,
+//       "nombre": "Artículo 2 Bis 36"
+//     },
+//     {
+//       "id": 640,
+//       "nombre": "Artículo 2 Bis 37"
+//     },
+//     {
+//       "id": 562,
+//       "nombre": "Artículo 2 Bis 22"
+//     },
+//     {
+//       "id": 599,
+//       "nombre": "Cobertura de riesgo de crédito"
+//     },
+//     {
+//       "id": 928,
+//       "nombre": "Artículo 2 Bis 90"
+//     },
+//     {
+//       "id": 496,
+//       "nombre": "Artículo 2 Bis 17"
+//     }
+//   ],
+//   "links": [
+//     {
+//       "id": 4800,
+//       "source": 2710,
+//       "type": "REFIERE",
+//       "target": 1211
+//     },
+//     {
+//       "id": 4398,
+//       "source": 1211,
+//       "type": "REFIERE",
+//       "target": 0
+//     },
+//     {
+//       "id": 4390,
+//       "source": 1202,
+//       "type": "REFIERE",
+//       "target": 1211
+//     },
+//     {
+//       "id": 3988,
+//       "source": 409,
+//       "type": "REFIERE",
+//       "target": 1211
+//     },
+//     {
+//       "id": 1330,
+//       "source": 1210,
+//       "type": "TIENE",
+//       "target": 1211
+//     },
+//     {
+//       "id": 4085,
+//       "source": 579,
+//       "type": "REFIERE",
+//       "target": 640
+//     },
+//     {
+//       "id": 4088,
+//       "source": 640,
+//       "type": "REFIERE",
+//       "target": 0
+//     },
+//     {
+//       "id": 4084,
+//       "source": 579,
+//       "type": "REFIERE",
+//       "target": 640
+//     },
+//     {
+//       "id": 4053,
+//       "source": 562,
+//       "type": "REFIERE",
+//       "target": 640
+//     },
+//     {
+//       "id": 4052,
+//       "source": 562,
+//       "type": "REFIERE",
+//       "target": 640
+//     },
+//     {
+//       "id": 633,
+//       "source": 599,
+//       "type": "TIENE",
+//       "target": 640
+//     },
+//     {
+//       "id": 4227,
+//       "source": 928,
+//       "type": "REFIERE",
+//       "target": 496
+//     },
+//     {
+//       "id": 4046,
+//       "source": 496,
+//       "type": "REFIERE",
+//       "target": 0
+//     }
+//   ]
+// }
 
 // the graph configuration, you only need to pass down properties
 // that you want to override, otherwise default ones will be used
@@ -156,7 +156,7 @@ const myConfig = {
   "nodeHighlightBehavior": true,
   "panAndZoom": false,
   "staticGraph": false,
-  "width": 1000,
+  "width": 1400,
   "d3": {
     "alphaTarget": 0.05,
     "gravity": -100,
@@ -204,172 +204,38 @@ class Grapho extends Component {
   
   constructor(props) {
     super(props);
-    // this.state = {
-    //   data : this.getDataAll()
-    // };
+    this.state = {
+      config : myConfig,
+      data :{
+          "nodes": [{
+                  "id": 777777777777777,
+                  "nombre": "Init Graph"
+                }],
+          "links": []
+      }
+    }
   }
-  
-  // getDataAll(){
-  //   var data = {
-  //     nodes : [],
-  //     links : []
-  //   }
-    
-  //   var session = driver.session();
-  //   session
-  //   .run('MATCH (a)-[r1]->(b) , (b)-[r2]->(c) WHERE NOT b.nombre="Artículo 1" OR  a.nombre="Artículo 1" OR c.nombre="Artículo 1" RETURN a,r1,b,r2,c LIMIT 5')
-  //   .then(function (result) {
-  //     result.records.forEach(function (record) {
-  //       // Nodos A
-  //       //console.log("Propiedades Nodo A:" , record.get('a'));
-  //       //console.log("Propiedades Nodo A:" , record.get('a').properties);
-  //       let dictionaryOfFirtstNodeProperties = record.get('a').properties
-  //       dictionaryOfFirtstNodeProperties['id'] = record.get('a').identity.low
-  //       //console.log("Resultado Nodo A: " , dictionaryOfFirtstNodeProperties);
-  //       if(data.nodes.filter(node => (node.id === dictionaryOfFirtstNodeProperties.id)).length == 0){
-  //         data.nodes.push(dictionaryOfFirtstNodeProperties)
-  //       }
-        
-  //       // Nodos B
-  //       //console.log("Propiedades Nodo B:" , record.get('b').properties);
-  //       let dictionaryOfSecondtNodeProperties = record.get('b').properties
-  //       dictionaryOfSecondtNodeProperties['id'] = record.get('b').identity.low
-  //       //console.log("Resultado Nodo B: " , dictionaryOfSecondtNodeProperties);
-  //       if(data.nodes.filter(node => (node.id === dictionaryOfSecondtNodeProperties.id)).length == 0){
-  //         data.nodes.push(dictionaryOfSecondtNodeProperties)
-  //       }
-        
-  //       // Nodos C
-  //       //console.log("Propiedades Nodo C:" , record.get('c').properties);
-  //       let dictionaryOfThirdNodeProperties = record.get('c').properties
-  //       dictionaryOfThirdNodeProperties['id'] = record.get('c').identity.low
-  //       //console.log("Resultado Nodo C: " , dictionaryOfThirdNodeProperties);
-  //       if(data.nodes.filter(node => (node.id === dictionaryOfThirdNodeProperties.id)).length == 0){
-  //         data.nodes.push(dictionaryOfThirdNodeProperties)
-  //       }
-        
-  //       // Relacion r1
-  //       let dictionaryOfRelationsR1 = {}
-  //       dictionaryOfRelationsR1['source'] = record.get('r1').start.low
-  //       dictionaryOfRelationsR1['target'] = record.get('r1').end.low
-  //       dictionaryOfRelationsR1['type'] = record.get('r1').type
-  //       dictionaryOfRelationsR1['id'] = record.get('r1').identity.low
-  //       if(data.links.filter(link => (link.id === dictionaryOfRelationsR1.id)).length == 0){
-  //         data.links.push(dictionaryOfRelationsR1)
-  //       }
-        
-  //       // Relacion r2
-  //       let dictionaryOfRelationsR2 = {}
-  //       dictionaryOfRelationsR2['source'] = record.get('r2').start.low
-  //       dictionaryOfRelationsR2['target'] = record.get('r2').end.low
-  //       dictionaryOfRelationsR2['type'] = record.get('r2').type
-  //       dictionaryOfRelationsR2['id'] = record.get('r2').identity.low
-  //       if(data.links.filter(link => (link.id === dictionaryOfRelationsR2.id)).length == 0){
-  //         data.links.push(dictionaryOfRelationsR2)
-  //       }
-  //     });
-  //     session.close();
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
-  //   console.log("DATA: " , data);
-  //   return data;
-  // }
 
-  // componentWillMount(){
-  //   var data = {
-  //     nodes : [],
-  //     links : []
-  //   }
-    
-  //   var session = driver.session();
-  //   session
-  //   .run('MATCH (a)-[r1]->(b) , (b)-[r2]->(c) WHERE NOT b.nombre="Artículo 1" OR  a.nombre="Artículo 1" OR c.nombre="Artículo 1" RETURN a,r1,b,r2,c LIMIT 5')
-  //   .then(function (result) {
-  //     result.records.forEach(function (record) {
-  //       // Nodos A
-  //       //console.log("Propiedades Nodo A:" , record.get('a'));
-  //       //console.log("Propiedades Nodo A:" , record.get('a').properties);
-  //       let dictionaryOfFirtstNodeProperties = record.get('a').properties
-  //       dictionaryOfFirtstNodeProperties['id'] = record.get('a').identity.low
-  //       //console.log("Resultado Nodo A: " , dictionaryOfFirtstNodeProperties);
-  //       if(data.nodes.filter(node => (node.id === dictionaryOfFirtstNodeProperties.id)).length == 0){
-  //         data.nodes.push(dictionaryOfFirtstNodeProperties)
-  //       }
-        
-  //       // Nodos B
-  //       //console.log("Propiedades Nodo B:" , record.get('b').properties);
-  //       let dictionaryOfSecondtNodeProperties = record.get('b').properties
-  //       dictionaryOfSecondtNodeProperties['id'] = record.get('b').identity.low
-  //       //console.log("Resultado Nodo B: " , dictionaryOfSecondtNodeProperties);
-  //       if(data.nodes.filter(node => (node.id === dictionaryOfSecondtNodeProperties.id)).length == 0){
-  //         data.nodes.push(dictionaryOfSecondtNodeProperties)
-  //       }
-        
-  //       // Nodos C
-  //       //console.log("Propiedades Nodo C:" , record.get('c').properties);
-  //       let dictionaryOfThirdNodeProperties = record.get('c').properties
-  //       dictionaryOfThirdNodeProperties['id'] = record.get('c').identity.low
-  //       //console.log("Resultado Nodo C: " , dictionaryOfThirdNodeProperties);
-  //       if(data.nodes.filter(node => (node.id === dictionaryOfThirdNodeProperties.id)).length == 0){
-  //         data.nodes.push(dictionaryOfThirdNodeProperties)
-  //       }
-        
-  //       // Relacion r1
-  //       let dictionaryOfRelationsR1 = {}
-  //       dictionaryOfRelationsR1['source'] = record.get('r1').start.low
-  //       dictionaryOfRelationsR1['target'] = record.get('r1').end.low
-  //       dictionaryOfRelationsR1['type'] = record.get('r1').type
-  //       dictionaryOfRelationsR1['id'] = record.get('r1').identity.low
-  //       if(data.links.filter(link => (link.id === dictionaryOfRelationsR1.id)).length == 0){
-  //         data.links.push(dictionaryOfRelationsR1)
-  //       }
-        
-  //       // Relacion r2
-  //       let dictionaryOfRelationsR2 = {}
-  //       dictionaryOfRelationsR2['source'] = record.get('r2').start.low
-  //       dictionaryOfRelationsR2['target'] = record.get('r2').end.low
-  //       dictionaryOfRelationsR2['type'] = record.get('r2').type
-  //       dictionaryOfRelationsR2['id'] = record.get('r2').identity.low
-  //       if(data.links.filter(link => (link.id === dictionaryOfRelationsR2.id)).length == 0){
-  //         data.links.push(dictionaryOfRelationsR2)
-  //       }
-  //     });
-  //     session.close();
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
-  //   this.setState({data: data})
-  //   console.log("DATA: " , data);
-  // }
-
-  // renderGraph(){
-  //   var datos = this.getDataAll()
-  //   console.log("ESTOS SON LOS DATOS: " ,datos)
-  //   return(
-  //     <Graph
-  //     id="graph-container" // id is mandatory, if no id is defined rd3g will throw an error
-  //     data={datos}
-  //     config={myConfig}
-  //     />
-  //   )
-  // }
+  componentDidMount() {
+    fetch("http://localhost:8080/articles/graph")
+      .then(response => response.json())
+      .then(data => {
+        console.log("Data del REST: " , data);
+        this.setState({data:data})
+      })
+  }
 
   render() {
-    // console.log("DATOS222: " , this.state.data)
-    // console.log("function: " , this.getDataAll())
-    // this.setState({ data: data })
+    const onMouseOverNode = function(node) {
+      console.log("Este es el nodo ID" , node);
+      
+    };
     return (
-      // <div>
-      // {this.renderGraph()}
-        
-      // </div>
       <Graph
       id="graph-container" // id is mandatory, if no id is defined rd3g will throw an error
-      data={data}
-      config={myConfig}
+      config={this.state.config}
+      data={this.state.data}
+      onMouseOverNode={onMouseOverNode}
       />
       )
     }
