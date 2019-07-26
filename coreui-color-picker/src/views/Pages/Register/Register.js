9e344008fb4f57bc2a0015fc6ae516bea05c604a
Leonardo <liogothicvampire@gmail.com>
2019-07-26 12:57:36 -0500

    Agregado coreui con color picker

--- coreui-color-picker/src/views/Pages/Register/Register.js
+++ coreui-color-picker/src/views/Pages/Register/Register.js
@@ -1,8 +1,56 @@
 import React, { Component } from 'react';
 import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import reactCSS from 'reactcss'
+import { SketchPicker } from 'react-color'
 
 class Register extends Component {
+  state = {
+    displayColorPicker: false,
+    color:'#ff0004',
+  };
+
+  handleClick = () => {
+    this.setState({ displayColorPicker: !this.state.displayColorPicker })
+  };
+
+  handleClose = () => {
+    this.setState({ displayColorPicker: false })
+  };
+
+  handleChange = (color) => {
+    this.setState({ color: color.hex })
+    console.log("Este es el color: " , this.state.color)
+  };
   render() {
+    const styles = reactCSS({
+      'default': {
+        color: {
+          width: '36px',
+          height: '14px',
+          borderRadius: '2px',
+          background: `${ this.state.color }`,
+        },
+        swatch: {
+          padding: '5px',
+          background: '#fff',
+          borderRadius: '1px',
+          boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+          display: 'inline-block',
+          cursor: 'pointer',
+        },
+        popover: {
+          position: 'absolute',
+          zIndex: '2',
+        },
+        cover: {
+          position: 'fixed',
+          top: '0px',
+          right: '0px',
+          bottom: '0px',
+          left: '0px',
+        },
+      },
+    });
     return (
       <div className="app flex-row align-items-center">
         <Container>
@@ -42,6 +90,23 @@ class Register extends Component {
                         </InputGroupText>
                       </InputGroupAddon>
                       <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <InputGroupAddon addonType="prepend">
+                        {/* <InputGroupText> */}
+                          {/* <i className="icon-lock"></i> */}
+                          Selecciona un color para la etiqueta
+                        {/* </InputGroupText> */}
+                      </InputGroupAddon>
+                      <div style={ styles.swatch } onClick={ this.handleClick }>
+          <div style={ styles.color } />
+        </div>
+        { this.state.displayColorPicker ? 
+          <div style={ styles.popover }>
+            <div style={ styles.cover } onClick={ this.handleClose }/>
+            <SketchPicker color={ this.state.color } onChange={ this.handleChange } />
+          </div> 
+          : null }
                     </InputGroup>
                     <Button color="success" block>Create Account</Button>
                   </Form>
