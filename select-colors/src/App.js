4b21fd1ba7e40cf5be1ab24ed2c9bc2249c4a9d0
Leonardo <liogothicvampire@gmail.com>
2019-08-20 11:17:49 -0500

    Front end implementation with select

---
+++ select-colors/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Selector from './SelectExample'
+
+function App() {
+  return (
+    <div className="App">
+      <p>Prueba de Select</p>
+      <Selector/>
+    </div>
+  );
+}
+
+export default App;
