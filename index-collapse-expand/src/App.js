170a3d82c869682ee0ce636295119a7a5351f3d8
Leonardo <liogothicvampire@gmail.com>
2019-12-11 12:59:28 -0600

    Expand Nodes on Click

--- index-collapse-expand/src/App.js
+++ index-collapse-expand/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SortableTree from 'react-sortable-tree';
+import SortableTree , {changeNodeAtPath} from 'react-sortable-tree';
 import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 
 class App  extends Component{
@@ -8,21 +8,50 @@ class App  extends Component{
 
     this.state = {
       treeData: [
-        { title: 'Secretaría de Hacienda y Crédito Público', children: [{ title: 'CNVB', children:[{title: 'CUB'}, {title: 'Fintech '}] }] },
-        { title: 'Secretaría de Finanzas', children: [{ title: 'Proximamente'}] },
-        { title: 'Secretaría de Economía', children: [{ title: 'Proximamente'}] },
-        { title: 'BANXICO', children: [{ title: 'Circular 13/2017'} , { title: 'Circular 14/2017'}] }
+        { id: 0 , title: 'Secretaría de Hacienda y Crédito Público', children: [{ title: 'CNVB', children:[{title: 'CUB'}, {title: 'Fintech '}] }] },
+        { id: 1 , title: 'Secretaría de Finanzas', children: [{ title: 'Proximamente'}] },
+        { id: 2 , title: 'Secretaría de Economía', children: [{ title: 'Proximamente'}] },
+        { id: 3 , title: 'BANXICO', children: [{ title: 'Circular 13/2017'} , { title: 'Circular 14/2017'}] }
       ],
+      nextTreeData: [
+        { title: 'Esto es lo próximo' },
+        { title: 'Ajua si se pudo'}
+      ]
     };
   }
 
   render() {
+    //const getNodeTitle = ({ node: { title } }) => title;
+    const getNodeKey = ({ node: { id } }) => id;
+
     return (
       <div style={{ height: 400 }}>
         <SortableTree
           treeData={this.state.treeData}
           onChange={treeData => this.setState({ treeData })}
           canDrag={false}
+          getNodeKey={getNodeKey}
+          generateNodeProps={ ({node , path}) => {
+
+            return{
+              onClick: () => {
+                console.log("On click: " , node.title)
+                console.log("Complete Node: " , node)
+                console.log("This is the Path: " , path)
+                const newIncomingData = this.state.nextTreeData
+                console.log(newIncomingData) 
+                this.setState(state => ({
+                  treeData: changeNodeAtPath({
+                    treeData: state.treeData,
+                    path,
+                    getNodeKey,
+                    newNode: { ...node, expanded: !node.expanded },
+                  }),
+                }));
+              }
+            }
+          }
+          }
         />
       </div>
     );
