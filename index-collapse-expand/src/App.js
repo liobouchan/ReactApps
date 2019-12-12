5f931a441441a6e696bd31df02c51ff5d4146c46
Leonardo <liogothicvampire@gmail.com>
2019-12-12 12:18:16 -0600

    Ready simulation an Lazy loading

--- index-collapse-expand/src/App.js
+++ index-collapse-expand/src/App.js
@@ -8,10 +8,10 @@ class App  extends Component{
 
     this.state = {
       treeData: [
-        { id: 0 , title: 'Secretaría de Hacienda y Crédito Público', children: [{ title: 'CNVB', children:[{title: 'CUB'}, {title: 'Fintech '}] }] },
-        { id: 1 , title: 'Secretaría de Finanzas', children: [{ title: 'Proximamente'}] },
-        { id: 2 , title: 'Secretaría de Economía', children: [{ title: 'Proximamente'}] },
-        { id: 3 , title: 'BANXICO', children: [{ title: 'Circular 13/2017'} , { title: 'Circular 14/2017'}] }
+        { id: 0 , title: 'Secretaría de Hacienda y Crédito Público', children: [{ id: 6 , title: 'CNVB', children:[{ id: 7 , title: 'CUB'}, {id: 8 ,title: 'Fintech '}] }] },
+        { id: 1 , title: 'Secretaría de Finanzas', children: [{ id: 9 , title: 'Proximamente'}] },
+        { id: 2 , title: 'Secretaría de Economía', children: [{ id: 10 , title: 'Proximamente'}] },
+        { id: 3 , title: 'BANXICO', children: [{ id: 11 , title: 'Circular 13/2017'} , { id: 12 ,title: 'Circular 14/2017'}] }
       ],
       nextTreeData: [
         { id: 5 , title: 'Esto es lo próximo' },
@@ -26,7 +26,7 @@ class App  extends Component{
     const getNodeKey = ({ node: { id } }) => id;
 
     return (
-      <div style={{ height: 800 }}>
+      <div style={{ height: '100vh' }}>
         <SortableTree
           treeData={this.state.treeData}
           onChange={treeData => this.setState({ treeData })}
@@ -35,28 +35,11 @@ class App  extends Component{
           generateNodeProps={ ({node , path}) => {
             return{
               onClick: () => {
-                console.log("On click: " , node.title)
-                console.log("Complete Node: " , node)
-                console.log("This is the Path: " , path)
                 const newIncomingData = this.state.nextTreeData
                 const parentKey = 0;
-                console.log(newIncomingData) 
-                if(newIncomingData){
-                  console.log("------------> Este es el nodo al que diste click: " , node)
-                  console.log("------------> Estos son los hijos: " , node.children)
-                  console.log("------------> Esto es la nueva info: " , newIncomingData)
-                  node.children.forEach((child) => {
-                    console.log("---------------------> Valor del children: ", child.title)
-                    newIncomingData.forEach((newData) => {
-                      console.log("------------------> Valor del NewData: " , newData.title)
-                    })
-                  })
-                                    
+                if(node.children == undefined || node.children.length <=  0){
+                  if(newIncomingData){
                   newIncomingData.forEach((newNode) => {
-                  console.log("Nuevo Nodo: " , newNode)
-                  console.log("parentKey: " , path)
-                  console.log("Que verga haces: " , path[path.length - 1])
-                  console.log("Este es el state: " , this.state.treeData)
                   this.setState(state => ({
                       treeData: addNodeUnderParent({
                         treeData: state.treeData,
@@ -68,6 +51,7 @@ class App  extends Component{
                       }).treeData,
                     }))
                   })
+                  }
                 }
               }
             }
