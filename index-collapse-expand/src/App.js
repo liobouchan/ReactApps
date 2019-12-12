f69766de3ad7a6f6e41325dfd0dc8c763186b0e5
Leonardo <liogothicvampire@gmail.com>
2019-12-11 18:21:29 -0600

    Solo falta una validáción

--- index-collapse-expand/src/App.js
+++ index-collapse-expand/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SortableTree , {changeNodeAtPath} from 'react-sortable-tree';
+import SortableTree , {changeNodeAtPath , addNodeUnderParent} from 'react-sortable-tree';
 import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 
 class App  extends Component{
@@ -14,9 +14,10 @@ class App  extends Component{
         { id: 3 , title: 'BANXICO', children: [{ title: 'Circular 13/2017'} , { title: 'Circular 14/2017'}] }
       ],
       nextTreeData: [
-        { title: 'Esto es lo próximo' },
-        { title: 'Ajua si se pudo'}
-      ]
+        { id: 5 , title: 'Esto es lo próximo' },
+        { id: 4 , title: 'Ajua si se pudo'}
+      ],
+      addAsFirstChild: false,
     };
   }
 
@@ -25,29 +26,49 @@ class App  extends Component{
     const getNodeKey = ({ node: { id } }) => id;
 
     return (
-      <div style={{ height: 400 }}>
+      <div style={{ height: 800 }}>
         <SortableTree
           treeData={this.state.treeData}
           onChange={treeData => this.setState({ treeData })}
           canDrag={false}
           getNodeKey={getNodeKey}
           generateNodeProps={ ({node , path}) => {
-
             return{
               onClick: () => {
                 console.log("On click: " , node.title)
                 console.log("Complete Node: " , node)
                 console.log("This is the Path: " , path)
                 const newIncomingData = this.state.nextTreeData
+                const parentKey = 0;
                 console.log(newIncomingData) 
-                this.setState(state => ({
-                  treeData: changeNodeAtPath({
-                    treeData: state.treeData,
-                    path,
-                    getNodeKey,
-                    newNode: { ...node, expanded: !node.expanded },
-                  }),
-                }));
+                if(newIncomingData){
+                  console.log("------------> Este es el nodo al que diste click: " , node)
+                  console.log("------------> Estos son los hijos: " , node.children)
+                  console.log("------------> Esto es la nueva info: " , newIncomingData)
+                  node.children.forEach((child) => {
+                    console.log("---------------------> Valor del children: ", child.title)
+                    newIncomingData.forEach((newData) => {
+                      console.log("------------------> Valor del NewData: " , newData.title)
+                    })
+                  })
+                                    
+                  newIncomingData.forEach((newNode) => {
+                  console.log("Nuevo Nodo: " , newNode)
+                  console.log("parentKey: " , path)
+                  console.log("Que verga haces: " , path[path.length - 1])
+                  console.log("Este es el state: " , this.state.treeData)
+                  this.setState(state => ({
+                      treeData: addNodeUnderParent({
+                        treeData: state.treeData,
+                        parentKey: path[path.length - 1],
+                        expandParent: false,
+                        getNodeKey,
+                        newNode: newNode,
+                        addAsFirstChild: state.addAsFirstChild,
+                      }).treeData,
+                    }))
+                  })
+                }
               }
             }
           }
