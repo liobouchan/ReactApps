7222b7deef9d54b209dffb884596d2223f53102f
Lio Bouchan <leo.bouchan@gmail.com>
2020-03-24 22:50:57 -0600

    Commit

---
+++ testing-auth-routes/src/Logeado.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Logeado extends React.Component   {
+    render(){
+        return(
+            <h1>Hola esto es para el Logeado</h1>
+        );
+    }
+}
+export default Logeado;
---
+++ testing-auth-routes/src/NoLogeado.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class NoLogeado extends React.Component   {
+    render(){
+        return(
+            <h1>Hola esto es para el QUE NO INICIÓ SESIÓN</h1>
+        );
+    }
+}
+export default NoLogeado;
